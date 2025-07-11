499fda78a97b76d597a68de518b4753ea2a729d4
Nimanivu <nimajoshy2004@gmail.com>
2025-07-11 15:44:53 +0530

    created

--- src/components/Deletebook.jsx
+++ src/components/Deletebook.jsx
@@ -6,8 +6,10 @@ const Deletebook = () => {
     <div>
         <Booknavbar/>
         <div className="container">
+          
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
                    <div className="row g-3">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                        
--- src/components/Viewallbook.jsx
+++ src/components/Viewallbook.jsx
@@ -1,139 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Booknavbar from './Booknavbar'
 
 const Viewallbook = () => {
+    const [bookData, changeBookData] = useState(
+        [
+            { "name": "Marvel", "description": "Comic", "Coverpage": "https://tse4.mm.bing.net/th/id/OIP.9-IwyumLzcQ2I-OXbPeyigHaLP?pid=Api&P=0&h=180" },
+            { "name": "Story Theives", "description": "Fiction", "Coverpage": "https://tse1.mm.bing.net/th/id/OIP.rY_GG6duh9isxrrcTI1_EwHaLM?pid=Api&P=0&h=180" },
+            { "name": "Avengers", "description": "Comic", "Coverpage": "https://tse4.mm.bing.net/th/id/OIP.5LFp1SyXWzIi5BFgK4ZV7wHaLF?pid=Api&P=0&h=180" },
+            { "name": "Those Eyes", "description": "Horor", "Coverpage": "https://tse3.mm.bing.net/th/id/OIP._oNdWRc8phreA1wqALiJmQHaLH?pid=Api&P=0&h=180" },
+            { "name": "Moon Light", "description": "Horor", "Coverpage": "https://tse3.mm.bing.net/th/id/OIP._80gRygjwAMX_czBc4g7fQHaKe?pid=Api&P=0&h=180" },
+            { "name": "Story Theives", "description": "Fiction", "Coverpage": "https://tse1.mm.bing.net/th/id/OIP.rY_GG6duh9isxrrcTI1_EwHaLM?pid=Api&P=0&h=180"},
+            { "name": "Avengers", "description": "Comic", "Coverpage": "https://tse4.mm.bing.net/th/id/OIP.5LFp1SyXWzIi5BFgK4ZV7wHaLF?pid=Api&P=0&h=180" },
+            { "name": "Those Eyes", "description": "Horor", "Coverpage": "https://tse3.mm.bing.net/th/id/OIP._oNdWRc8phreA1wqALiJmQHaLH?pid=Api&P=0&h=180" }
+
+        ]
+    )
     return (
         <div>
-            <Booknavbar/>
+            <Booknavbar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
 
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            {bookData.map(
+                                (value, index) => {
+                                    return (
+                                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
+                                            <div class="card" >
+                                                <img src={value.Coverpage} class="card-img-top" alt="..." />
+                                                <div class="card-body">
+                                                    <h5 class="card-title">{value.name}</h5>
+                                                    <p class="card-text">{value.description}</p>
+                                                    <a href="#" class="btn btn-primary">Buy Now</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
 
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                }
+                            )}
 
 
-
-
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-
-
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-
-
-
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-
-
-
-
-
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-
-
-
-
-
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-
-
-
-                                <div class="card" >
-                                    <img src="https://tse2.mm.bing.net/th/id/OIP.J5JGO_6LOA98xVegnKkOPwHaJu?pid=Api&P=0&h=180" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Comic</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                                        <a href="#" class="btn btn-primary">Buy Now</a>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
